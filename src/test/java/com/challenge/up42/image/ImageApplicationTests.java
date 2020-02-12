a4d30bc7b210a49c780a1d6722e652db4dacb1c4
Ahsan Qamar <ahsan.qamar@circ.com>
2020-02-12 13:53:09 +0100

    init the project

---
+++ src/test/java/com/challenge/up42/image/ImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.up42.image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
